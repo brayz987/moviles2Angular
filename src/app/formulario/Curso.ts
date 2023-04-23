b833066719efb24a34a21ce6d464aa91a4f226c7
Brayan Cardenas <dev5@dyalogo.com>
2023-04-22 21:28:43 -0500

    tarea terminada

---
+++ src/app/formulario/Curso.ts
@@ -0,0 +1,21 @@
+export class Curso {
+    private curso: string[];
+    private repuesta: string;
+
+    constructor(){
+        this.curso = ['Java1','Java2','Oracle','PHP1','PHP2','Javascript','TypeScript'];
+        this.repuesta = "";
+    }
+
+    public getCurso():string[] {
+        return this.curso;
+    }
+
+    public getRespuesta():string {
+        return this.repuesta;
+    }
+
+    public inscribir(nombre:string, edad:string, curso:string, direccion: string){
+        this.repuesta = `Se ha inscrito al curso: ${curso}, Estudiante: ${nombre} edad: ${edad} direccion: ${direccion}`;
+    }
+}
